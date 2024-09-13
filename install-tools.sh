6fa36d19a1565873042fd46d0eca3eeb2442f976
Garth Pickell <garth.pickell@vertigis.com>
2024-09-13 12:30:09 -0700

    *** adding more setup code

--- install-tools.sh
+++ install-tools.sh
@@ -2,16 +2,19 @@
 set -e
 
 mkdir -p .temp
-sudo apt -y update
-sudo apt -y upgrade
-sudo apt -y install nano
+apt -y update
+apt -y upgrade
+apt -y install nano
 
 if ! which az > /dev/null; then
   curl -fsSL https://aka.ms/InstallAzureCLIDeb -o .temp/get-az.sh
-  sudo bash ./.temp/get-az.sh
+  bash ./.temp/get-az.sh
 fi
 
 if ! which docker > /dev/null; then
   curl -fsSL https://get-docker.com -o .temp/get-docker.sh
-  sudo bash ./.temp/get-docker.sh
+  bash ./.temp/get-docker.sh
 fi
+
+rm -rf .temp
+usermod -aG docker $SUDO_USER
